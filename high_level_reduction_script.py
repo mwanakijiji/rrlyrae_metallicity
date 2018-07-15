6921478a9299d3a51dd988c3b8dcbc984c568c21
Eckhart <spalding@email.arizona.edu>
2018-07-14 18:19:45 -0700

    Synthetic spectra + normalization working

--- high_level_reduction_script.py
+++ high_level_reduction_script.py
@@ -1,23 +1,23 @@
-# GENERATE OUR SOLUTION
-# NEED:
-# 1. FE/H VALUES FROM RESCALED HIGH-RES DATA FROM LITERATURE
-# 2. SPECTRA PHASE VALUES
-# 3. OUR OWN EW DATA, WITH CORRXNS FOR CA ABSORPTION
+from modules2 import *  # import stuff in init file
+from modules2 import create_spec_realizations #norm_spec
 
-norm_spec
+# normalize spectra for making the calibration in the first place (no! not first step! first we need to generate synthetic spectra, and THEN normalize)
+#mamluk = norm_spec.norm_spec("in.list") # create instance 
+#mamluk() # call instance
 
-generate_synthetic_spec
+#generate_synthetic_spec
+create_spec_realizations.create_spec_realizations_main("spec_names.list", synthetic_out_dir)
 
-run_robospect
+#run_robospect
 
-scrape_ew_from_robo
+#scrape_ew_from_robo
 
-apply_interstellar_ca_absorption
+#apply_interstellar_ca_absorption
 
-findHK
+#findHK
 
-read_lit_metallicities
+#read_lit_metallicities
 
-rescale_lit_metallicities
+#rescale_lit_metallicities
 
-run_emcee
+#run_emcee
--- setup.py
+++ setup.py
@@ -2,21 +2,37 @@
 # C++ (for Carrell's script)
 # Python (for the wrapper in the first place)
 # GSL ver1 as the libgsl0-dev package (for Robospect)
-# maybe Fortran (for RW's code)
 
 from setuptools import setup, Extension
+from setuptools.command.install import install
+import shlex
+import subprocess
 #from numpy.distutils.core import setup, Extension
 
-module1 = Extension('bkgrnd_module', sources = ['bkgrnd.cc'])
-#module2 = Extension('hello_fortran', sources = ['wrapper/hello_fortran.f'])
+'''
+class CustomInstall(install):
+    def run(self):
+        command = "g++ -o bkgrnd7 bkgrnd.cc"
+        normzn_compile = shlex.split(command)
+        process = subprocess.run(normzn_compile) #, shell=True, stderr=subprocess.STDOUT)
+        #process = subprocess.Popen(normzn_compile, shell=True, stderr=subprocess.STDOUT)
+        #process.wait()
+        print('allo?')
+        print('_')
+        print('_')
+        print('_')
+        install.run(self)
+'''               
+        
+module1 = Extension("bkgrnd", sources = ["bkgrnd.cc"])
 
-
-setup(name='RRab metallicity',
-      version='1.0',
-      description='For finding FeH from low-res survey spectra of RRab stars',
-      author='Eckhart Spalding, Ron Wilhelm, Nathan De Lee, Kenneth Carrell',
-      author_email='spalding at email dot arizona dot edu',
-      url='https://github.com/mwanakijiji/rrlyrae_metallicity',
-      license='MIT',
-      ext_modules=[module1],
-      zip_safe=False)
+setup(name="RRab metallicity",
+      version="1.0.0",
+      description="For finding FeH from low-res survey spectra of RRab stars",
+      author="Eckhart Spalding, Ron Wilhelm, Nathan De Lee, Kenneth Carrell",
+      author_email="spalding at email dot arizona dot edu",
+      url="https://github.com/mwanakijiji/rrlyrae_metallicity",
+      license="MIT",
+      include_package_data=True,
+      ext_modules=[module1]
+      ) # cmdclass={'install': CustomInstall}
