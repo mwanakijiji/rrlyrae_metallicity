609223c2caad02d6802199fa65d90648d04ba652
Eckhart <spalding@email.arizona.edu>
2018-08-18 16:27:33 -0700

    Added more scripts to be imported as packages by setup.py

--- setup.py
+++ setup.py
@@ -12,30 +12,15 @@ from subprocess import Popen,PIPE
 
 long_description = "For determining metallicities of RR Lyraes from low-res spectroscopy see `here <https://github.com/mwanakijiji/rrlyrae_metallicity>`__ for more info"
 
-# check --compile-background flag
-# if true, compile Carrell's normalization script
-
-'''
-try:
-    compile_back = sys.argv.index('--compile-background')
-except ValueError:
-    _COMPILE_BACKGROUND = False
-else:
-    del sys.argv[compile_back]
-    _COMPILE_BACKGROUND = True
-if _COMPILE_BACKGROUND:
-    bkgrnd_compile = Popen(["g++","-o","bkgrnd351","bkgrnd.cc"],stdout=PIPE,stderr=PIPE)
-'''
-    
-#bkgrnd_compile = Popen(["g++","-o","bkgrnd351","bkgrnd.cc"],stdout=PIPE,stderr=PIPE)
     
 setup(name="RRab metallicity",
-      version="1.0.0",
+      version="0.0.1",
       description="For finding FeH from low-res survey spectra of RRab stars",
       long_description=long_description,
       author="Eckhart Spalding, Ron Wilhelm, Nathan De Lee, Kenneth Carrell",
       author_email="spalding at email dot arizona dot edu",
       url="https://github.com/mwanakijiji/rrlyrae_metallicity",
       license="MIT",
+      packages=['rrlyrae_metallicity','rrlyrae_metallicity/modules2'],
       include_package_data=True
       )
