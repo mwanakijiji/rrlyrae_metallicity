671850c987eca0efeb12f2977e388f1b38c2293f
Eckhart <spalding@email.arizona.edu>
2019-03-25 17:58:06 -0700

    compile_bkgrnd() seems to work

--- rrlyrae_metallicity/modules2/compile_normalization.py
+++ rrlyrae_metallicity/modules2/compile_normalization.py
@@ -7,17 +7,27 @@ from modules2 import *
 Compile spectral normalization script
 '''
 
+# configuration data
+config = configparser.ConfigParser() # for parsing values in .init file
+config.read("rrlyrae_metallicity/modules2/config.ini")
+
 def compile_bkgrnd():
     _COMPILE_BKGRND = True
     if _COMPILE_BKGRND:
         if True:
+            
             print("Compiling background normalization script...")
-            bkgrnd_compile = Popen(["g++","-o","./rrlyrae_metallicity/bin/bkgrnd","./rrlyrae_metallicity/src/bkgrnd.cc"],stdout=PIPE,stderr=PIPE)
-            # now move the executable to the right path... but what if I'm not using a conda environment? I won't know where to move it, beyond just get_setuptools_script_dir()
-            shutil.copy("./rrlyrae_metallicity/bin/bkgrnd",get_setuptools_script_dir()) # put copy of executable in Python setup tools path
+            bkgrnd_compile = Popen(["g++","-o",
+                                    config["data_dirs"]["DIR_BIN"] + "bkgrnd",
+                                    config["data_dirs"]["DIR_SRC"] + "bkgrnd.cc"],
+                                    stdout=PIPE,stderr=PIPE)
+            
+            # now move the executable to the right path (the Python setup tools path)
+            # but what if I'm not using a conda environment? I won't know where to move it, beyond just get_setuptools_script_dir()
+            shutil.copy(config["data_dirs"]["DIR_BIN"] + "bkgrnd",
+                        get_setuptools_script_dir()) 
         try:
-            check_call(["rm","-rf","./rrlyrae_metallicity/bin/bkgrnd"]) # (why is removal of original file not working without throwing an error??)
+            # (why is removal of original file not working without throwing an error??)
+            check_call(["rm","-rf",config["data_dirs"]["DIR_BIN"] + "bkgrnd"]) 
         except CalledProcessError:
             print("Binary bkgrnd not removed from original location")
-
-    
