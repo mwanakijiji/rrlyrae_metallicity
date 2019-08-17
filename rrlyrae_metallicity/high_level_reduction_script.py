b8575ea6eed42ff4d5fcbcc92d998e54f65e30e0
Eckhart <spalding@email.arizona.edu>
2019-08-17 13:13:04 -0700

    Ran Pylint on high_level*py

--- rrlyrae_metallicity/high_level_reduction_script.py
+++ rrlyrae_metallicity/high_level_reduction_script.py
@@ -1,6 +1,9 @@
+'''
+This is the high-level script which runs all the pieces of the pipeline
+'''
+
 import sys
-import os
-from modules2 import * 
+from modules2 import *
 from modules2 import \
      compile_normalization, \
      create_spec_realizations, \
@@ -10,25 +13,23 @@ from modules2 import \
      consolidate_pre_mcmc, \
      run_emcee, \
      error_propagation_and_mapping
-from subprocess import Popen,PIPE
-import ipdb
-
-########################
-## MAKE THE SOLUTION IN THE FIRST PLACE; WORRY ABOUT APPLYING IT LATER
-########################
 
 def main():
 
-    import ipdb; ipdb.set_trace()
+    #import ipdb; ipdb.set_trace()
     # Make all the directories
-    make_dirs(type = "find_abcd")
-    
-    # Compile the C spectral normalization script
-    #compile_normalization.compile_bkgrnd()
+    '''
+    make_dirs(type = "find_abcd") # find_abcd as opposed to apply_abcd
 
+    #import ipdb; ipdb.set_trace()
+    # Compile the C spectral normalization script
+    compile_normalization.compile_bkgrnd()
+
+    #import ipdb; ipdb.set_trace()
     # Take list of unnormalized empirical spectra and generate noise-churned spectra
-    #create_spec_realizations.create_spec_realizations_main(num = 2) ## ## just 2 realizations for testing (otherwise, 100)
-    
+    ## ## just 2 realizations for testing (otherwise, 100)
+    create_spec_realizations.create_spec_realizations_main(num = 2)
+
     # run_robospect on normalized synthetic spectra
     ## ## IMPLEMENT THE PYTHON VERSION OF ROBOSPECT WHEN ITS OUT
     #run_robo.run_robospect()
@@ -40,23 +41,26 @@ def main():
     # find equivalent widths
     #find_HK_instance = scrape_ew_and_errew.findHK() # instantiate Balmer, CaIIK finder
     #find_HK_instance() # call instance
-    
+
     # apply_interstellar_ca_absorption (needed?)
     ## ## ca_correction.ca_corrxn("maps_EW(CaNa)_20150318.fits")
 
-    # apply offsets to Fe/H values, etc., to map Fe/H values based on a basis set, and pickle results
+    # apply offsets to Fe/H values, etc., to map Fe/H values based
+    # on a basis set, and pickle results
     make_high_res_feh_basis.calc_FeH_program_stars()
-    
+
     # bootstrap to obtain mapped Fe/H values with errors
     #error_propagation_and_mapping.FeHmapper().do()
-    
-    # consolidate data to feed into the MCMC: K and Balmer EWs, mapped Fe/Hs, errors; data is also winnowed based on phase
+    '''
+    # consolidate data to feed into the MCMC: K and Balmer EWs, mapped Fe/Hs,
+    # errors; data is also winnowed based on phase
     consolidate_pre_mcmc.graft_feh() # graft mapped FeH values onto table of EWs
-    consolidate_pre_mcmc.winnow_by_phase_type(remove_rrl_subtype = "c") # remove data corresponding to bad phase values, wrong type
-    
+    # remove data corresponding to bad phase values, wrong type
+    consolidate_pre_mcmc.winnow_by_phase_type(remove_rrl_subtype="c")
+
     # run_emcee with input data_table_winnowed
     emcee_instance = run_emcee.RunEmcee()
-    emcee_instance() # call instance    
+    emcee_instance() # call instance
 
 
 # entry point
