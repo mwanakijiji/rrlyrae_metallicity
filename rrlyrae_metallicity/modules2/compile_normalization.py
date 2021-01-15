ab0d2d65716a57e2067c1266dcb5e5c037c11661
Eckhart Spalding <bandari@eckharts-mbp.dhcp.nd.edu>
2021-01-15 13:37:46 -0500

    just before removing abs path

--- rrlyrae_metallicity/modules2/compile_normalization.py
+++ rrlyrae_metallicity/modules2/compile_normalization.py
@@ -13,30 +13,35 @@ def compile_bkgrnd(objective = "apply_abcd"):
         config_choice = config_apply
     elif (objective == "find_abcd"):
         config_choice = config
-    
+
     _COMPILE_BKGRND = True
     if _COMPILE_BKGRND:
         if True:
 
-            print("--------------------------")
-            print("Compiling background normalization script...")
+            cc_path_abs = config_choice["data_dirs"]["DIR_SRC"] + "/bkgrnd.cc"
+            compile_to_temp_path_abs = config_choice["data_dirs"]["DIR_BIN"] + "/bkgrnd"
+
+            logging.info("--------------------------")
+            logging.info("Compiling background normalization script...")
             bkgrnd_compile = Popen(["g++", "-o",
-                                    config_choice["data_dirs"]["DIR_BIN"] + "bkgrnd",
-                                    config_choice["data_dirs"]["DIR_SRC"] + "bkgrnd.cc"],
+                                    compile_to_temp_path_abs,
+                                    cc_path_abs],
                                     stdout=PIPE, stderr=PIPE)
-            print(get_setuptools_script_dir())
+            logging.info(get_setuptools_script_dir())
             # now move the executable to the right path (the Python setup tools path)
             # but what if I'm not using a conda environment? I won't know where to move
             # it, beyond just get_setuptools_script_dir()
-            shutil.copy(config_choice["data_dirs"]["DIR_BIN"] + "bkgrnd",
+            print("yada1")
+            shutil.copy(compile_to_temp_path_abs,
                         get_setuptools_script_dir())
+            print("yada2")
 
-            print("Binary for spectrum normalization saved to")
-            print(config_choice["data_dirs"]["DIR_BIN"] + "bkgrnd")
-            print("--------------------------")
+            logging.info("Binary for spectrum normalization saved to")
+            logging.info(get_setuptools_script_dir())
+            logging.info("--------------------------")
 
         try:
             # (why is removal of original file not working without throwing an error??)
-            check_call(["rm", "-rf", get_setuptools_script_dir() + "bkgrnd"])
+            check_call(["rm", "-rf", compile_to_temp_path_abs])
         except CalledProcessError:
             print("Binary bkgrnd not removed from original location")
