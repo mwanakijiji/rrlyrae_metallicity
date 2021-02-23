a92f5e57903be6f4a6f712bca2ed0ca1fb70c893
Eckhart Spalding <espaldin@nd.edu>
2021-02-23 15:41:07 -0500

    revamping the run_emcee part

--- rrlyrae_metallicity/high_level_reduction_script.py
+++ rrlyrae_metallicity/high_level_reduction_script.py
@@ -20,44 +20,34 @@ def main():
     # make all the directories
 
     make_dirs(objective = "find_abcd") ## find_abcd as opposed to apply_abcd
-
+    '''
     # compile the C spectral normalization script
     compile_normalization.compile_bkgrnd()
 
     # Take list of unnormalized empirical spectra and noise-churned the
-    # spectra, normalize them, and write them out
+    # spectra, normalize them, and write out normalizations
     ## ## just 1 or 2 realizations for testing (default is 100)
     create_spec_realizations.create_spec_realizations_main(num = 1, noise_level=0)
-    '''
+
     # run_robospect on normalized synthetic spectra
     run_robo.main()
 
     # scrape_ew_from_robo and calculate EWs + err_EW
+
     scraper_instance = scrape_ew_and_errew.Scraper()
     scraper_instance() # call instance
-
     scrape_ew_and_errew.quality_check()
 
-    # find net K, H equivalent widths and make K-H plot
-    find_HK_instance = scrape_ew_and_errew.findHK()
-    find_HK_instance() # call instance
-
-    # IF FEH IN LIST_SPEC_PHASE ARE -999:
-    # apply offsets to Fe/H values, etc., to map Fe/H values based
-    # on a basis set, and pickle results (IF FeH is being calculated for empirical spectra)
-    make_high_res_feh_basis.calc_feh_program_stars()
-
-    # bootstrap to obtain mapped Fe/H values with errors
-    error_propagation_and_mapping.feh_mapper().do()
-
-    # graft mapped FeH values onto table of EWs
-    #consolidate_pre_mcmc.graft_feh() # this is for empirical spectra, which have a Fe/H basis
-    consolidate_pre_mcmc.graft_feh(synthetic=True) # this is for synthetic spectra, which have Fe/H in the file name
-    # remove data corresponding to bad phase values, wrong type
-    consolidate_pre_mcmc.winnow()
+    # put the good EW data into a table with
+    # rows corresponding to files and cols for the lines
+    scrape_ew_and_errew.stack_spectra()
+    '''
 
     # run_emcee with input data_table_winnowed
-    emcee_instance = run_emcee.RunEmcee()
+    # coeff defs: K = a + bH + cF + dHF + f(H^2) + g(F^2) + h(H^2)F + kH(F^2) + m(H^3) + n(F^3)
+    # where K is CaII K EW; H is Balmer EW; F is [Fe/H]
+    emcee_instance = run_emcee.RunEmcee(coeffs=['a','b','c','d','f','g','h','k'])
+    '''
     emcee_instance() # call instance
     '''
 
