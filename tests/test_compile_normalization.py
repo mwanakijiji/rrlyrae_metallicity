1ed09b8487878611af47b79d2d9579553b66b546
Eckhart Spalding <espaldin@nd.edu>
2021-08-03 15:27:45 -0400

    test patch

--- tests/test_compile_normalization.py
+++ tests/test_compile_normalization.py
@@ -15,11 +15,10 @@ from rrlyrae_metallicity.rrlyrae_metallicity import *
 from rrlyrae_metallicity.rrlyrae_metallicity.modules2 import *
 
 @patch('builtins.print')
-def test_compile_bkgrnd():
+def test_compile_bkgrnd(self):
     # does bkgrnd compile?
 
-    final_list = compile_bkgrnd(compiled_bkgrnd_file_path_abs_pass=compiled_bkgrnd_file_path_abs,
-                                cc_bkgrnd_file_path_abs_pass=cc_bkgrnd_file_path_abs)
+    final_list = compile_bkgrnd()
 
     # check that what would be printed is what we expect upon
     # successful compilation
