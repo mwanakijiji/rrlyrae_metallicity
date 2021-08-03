b0611c3f752cdebfb9cc9fbda2fcbb0150d0ecdd
Eckhart Spalding <espaldin@nd.edu>
2021-08-03 18:42:57 -0400

    test

--- tests/test_compile_normalization.py
+++ tests/test_compile_normalization.py
@@ -29,7 +29,7 @@ def test_compile_bkgrnd():
     #root_dir_test = $GITHUB_WORKSPACE
     compile_status = modules2.compile_normalization.compile_bkgrnd(
         compiled_bkgrnd_file_path_abs_pass = root_dir_test+"src/"+os.path.basename(compiled_bkgrnd_file_path_abs),
-        cc_bkgrnd_file_path_abs_pass = root_dir_test+"src/"+os.path.basename(cc_bkgrnd_file_path_abs)
+        cc_bkgrnd_file_path_abs_pass = "src/bkgrnd.cc"
         )
 
     #with patch('sys.stdout.readlines()[-1]', new = StringIO()) as fake_out:
