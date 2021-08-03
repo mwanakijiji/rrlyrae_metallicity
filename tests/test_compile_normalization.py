d3ac3df4b7f509b27929b87dabbf0f0267d4f9a9
Eckhart Spalding <espaldin@nd.edu>
2021-08-03 15:36:49 -0400

    test patch

--- tests/test_compile_normalization.py
+++ tests/test_compile_normalization.py
@@ -10,7 +10,7 @@ print(current_dir)
 print(target_dir)
 sys.path.insert(0, target_dir)
 
-from rrlyrae_metallicity.rrlyrae_metallicity import modules2
+from rrlyrae_metallicity.rrlyrae_metallicity import modules2.compile_normalization
 #from modules2 import compile_normalization
 from rrlyrae_metallicity.rrlyrae_metallicity import *
 from rrlyrae_metallicity.rrlyrae_metallicity.modules2 import *
@@ -19,7 +19,7 @@ from rrlyrae_metallicity.rrlyrae_metallicity.modules2 import *
 def test_compile_bkgrnd(self):
     # does bkgrnd compile?
 
-    final_list = compile_normalization.compile_bkgrnd()
+    final_list = modules2.compile_normalization.compile_bkgrnd()
 
     # check that what would be printed is what we expect upon
     # successful compilation
