9a861c964676ad0f7aa341008cf81228f381e744
Eckhart Spalding <espaldin@nd.edu>
2021-10-26 20:51:41 -0400

    removed code with # pragma: no cover

--- modules/teff_retrieval.py
+++ modules/teff_retrieval.py
@@ -49,9 +49,10 @@ def line_fit_temp_range(x_data_pass, y_data_pass, t_min, t_max):
 
 def temp_vs_balmer(df_poststack_file_name_read = config_red["data_dirs"]["DIR_EW_PRODS"]+config_red["file_names"]["RESTACKED_EW_DATA_W_METADATA"],
                     df_poststack_file_name_write = config_red["data_dirs"]["DIR_EW_PRODS"] + config_red["file_names"]["RESTACKED_EW_DATA_GOOD_ONLY_TEFFFIT"],
-                    plot_write = config_red["data_dirs"]["DIR_BIN"] + config_red["file_names"]["TEFF_VS_BALMER"],
                     t_min=5900,
-                    t_max=7350):
+                    t_max=7350,
+                    plot_write = config_red["data_dirs"]["DIR_BIN"] + config_red["file_names"]["TEFF_VS_BALMER"],
+                    plot=True):
     '''
     Finds a linear Teff vs. Balmer EW relation. This is an ancillary step before
     running the MCMC further downstream in the pipeline.
@@ -62,9 +63,10 @@ def temp_vs_balmer(df_poststack_file_name_read = config_red["data_dirs"]["DIR_EW
         and 'EW_Balmer', with which a simple linear fit is made
     df_poststack_file_name_write: name of file to write; this file is the same as
         the one read in, except that now it also includes the best-fit values of the Teff
-    plot_write: file name of Teff vs Balmer plot to write
     t_min: the minimum Teff of spectra that the linear fit will be made to
     t_max: the maximum "  "  "  "
+    plot_write: file name of Teff vs Balmer plot to write
+    plot: flag whether to write plot or not
 
     OUTPUTS:
     m:      slope
@@ -98,20 +100,20 @@ def temp_vs_balmer(df_poststack_file_name_read = config_red["data_dirs"]["DIR_EW
     logging.info("Wrote out data file including linear-best-fit Teffs to " + df_poststack_file_name_write)
 
     # save an FYI plot
-    plt.clf()
-    plt.title("Teff from the Balmer EW\n[m, err_m, b, err_b] = \n" +
-                "[" + str(np.round(m,2)) + ", " + str(np.round(err_m,2)) + ", " + str(np.round(b,2)) + ", " + str(np.round(err_b,2)) + "]")
-    plt.axhline(y=t_max, color="k", alpha=0.4)
-    plt.axhline(y=t_min, color="k", alpha=0.4)
-    plt.plot(ews_Balmer, teffs_bestfit, linestyle='--')
-    plt.scatter(ews_Balmer, teff, color="k", s=3)
-    #plt.ylim([0,70])
+    if plot: # pragma: no cover
+        plt.clf()
+        plt.title("Teff from the Balmer EW\n[m, err_m, b, err_b] = \n" +
+                    "[" + str(np.round(m,2)) + ", " + str(np.round(err_m,2)) + ", " + str(np.round(b,2)) + ", " + str(np.round(err_b,2)) + "]")
+        plt.axhline(y=t_max, color="k", alpha=0.4)
+        plt.axhline(y=t_min, color="k", alpha=0.4)
+        plt.plot(ews_Balmer, teffs_bestfit, linestyle='--')
+        plt.scatter(ews_Balmer, teff, color="k", s=3)
 
-    plt.ylabel("Teff (K)")
-    plt.xlabel("EW (Angstr)")
-    plt.tight_layout()
-    plt.savefig(plot_write)
-    plt.clf()
-    logging.info("Wrote out plot of Teffs vs. Balmer EW to " + plot_write)
+        plt.ylabel("Teff (K)")
+        plt.xlabel("EW (Angstr)")
+        plt.tight_layout()
+        plt.savefig(plot_write)
+        plt.clf()
+        logging.info("Wrote out plot of Teffs vs. Balmer EW to " + plot_write)
 
     return df_poststack
